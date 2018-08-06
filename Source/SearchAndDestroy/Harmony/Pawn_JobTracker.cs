ff8d53b9b96605679091e55c2aa677677c321c91
Roel <roelheirman@gmail.com>
2018-08-06 10:35:10 +0200

    manual orders now cancel s&d mode + added gitignore

---
+++ Source/SearchAndDestroy/Harmony/Pawn_JobTracker.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace SearchAndDestroy.Harmony
+{
+    [HarmonyPatch(typeof(Pawn_JobTracker), "DetermineNextJob")]
+    static class Pawn_JobTracker_DetermineNextJob
+    {
+        static void Postfix(Pawn_JobTracker __instance, ref ThinkResult __result)
+        {
+            Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+
+            if (pawn.Drafted)
+            {
+                Log.Message("started new job for pawn: " +  pawn.Name);
+            }
+
+        }
+    }
+}
